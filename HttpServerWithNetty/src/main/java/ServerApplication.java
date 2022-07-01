428d340dd8063f5ba88ce5c8ce5c7df302e990af
ruitianzhong <zhongruitian2003@qq.com>
2022-07-01 22:42:51 +0800

    Add META-INF to specify the Main-Class

--- HttpServerWithNetty/src/main/java/ServerApplication.java
+++ HttpServerWithNetty/src/main/java/ServerApplication.java
@@ -11,17 +11,17 @@ public class ServerApplication {
 
     public static void main(String[] args) throws IOException, URISyntaxException {
         if (args.length != 2) {
-            Server server = new Server(new HttpChannelHandlerInitializer(),DEFAULT_PORT);
+            Server server = new Server(new HttpChannelHandlerInitializer(), DEFAULT_PORT);
             server.run();
-        }else if(args[0].equals("-p")||args.equals("--port")){
+        } else if (args[0].equals("-p") || args.equals("--port")) {
             int port = Integer.valueOf(args[1]);
-            if(port<=0){
+            if (port <= 0) {
                 System.out.println("Port number must be larger than ZERO!");
                 exit(-1);
             }
-            Server server = new Server(new HttpChannelHandlerInitializer(),port);
+            Server server = new Server(new HttpChannelHandlerInitializer(), port);
             server.run();
-        }else {
+        } else {
             System.out.println("Usage:--port 8080 to specify the port number");
         }
     }
