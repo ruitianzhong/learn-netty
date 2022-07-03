ed960c216e7e490d1a9e28dc5bab5a658f86ef87
ruitianzhong <zhongruitian2003@qq.com>
2022-07-03 18:58:45 +0800

    ConfigurationValues Constants

---
+++ HttpServerWithNetty/src/main/java/top/zhongruitian/ServerWithNetty/Utils/ConfigValueConstants.java
@@ -0,0 +1,8 @@
+package top.zhongruitian.ServerWithNetty.Utils;
+
+public class ConfigValueConstants {
+    public static final String PORT = "10086";
+    public static final String INDEX = "index.html,home.html";
+    public static final long TIME = 10000;
+    public static final String FORBIDDEN_EXTENSIONS = "out,c,properties";
+}
--- HttpServerWithNetty/src/main/java/top/zhongruitian/ServerWithNetty/Utils/DefaultPropertiesFactory.java
+++ HttpServerWithNetty/src/main/java/top/zhongruitian/ServerWithNetty/Utils/DefaultPropertiesFactory.java
@@ -4,19 +4,20 @@
 package top.zhongruitian.ServerWithNetty.Utils;
 
 import java.util.Properties;
+
 /**
-@author ruitianzhong
-@email zhongruitian2003@qq.com
-@date 2022/7/3 18:26
-@description 
-*/
+ * @author ruitianzhong
+ * @email zhongruitian2003@qq.com
+ * @date 2022/7/3 18:26
+ * @description
+ */
 
 public class DefaultPropertiesFactory {
-    public static Properties getDefaultProperties(){
+    public static Properties getDefaultProperties() {
         Properties properties = new Properties();
-        properties.setProperty("server.port","10086");
-        properties.setProperty("server.time","200");
-        properties.setProperty("server.index","index.html,home.html");
+        properties.setProperty(ConfigurationPrefix.PORT, ConfigValueConstants.PORT);
+        properties.setProperty(ConfigurationPrefix.TIME, ConfigValueConstants.INDEX);
+        properties.setProperty(ConfigurationPrefix.INDEX, ConfigValueConstants.INDEX);
         return properties;
     }
 }
