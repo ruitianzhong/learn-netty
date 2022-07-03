10535b2aad93f6f4507072457554af309ac3f565
ruitianzhong <zhongruitian2003@qq.com>
2022-07-03 18:42:35 +0800

    ConfigurationPrefix Constants

---
+++ HttpServerWithNetty/src/main/java/top/zhongruitian/ServerWithNetty/Utils/ConfigurationPrefix.java
@@ -0,0 +1,7 @@
+package top.zhongruitian.ServerWithNetty.Utils;
+
+public class ConfigurationPrefix {
+    public static final String PORT = "server.port";
+    public static final String INDEX = "server.index";
+    public static final String TIME = "server.time";
+}
