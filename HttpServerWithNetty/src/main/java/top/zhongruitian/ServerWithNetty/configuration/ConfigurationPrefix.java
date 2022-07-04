943b5cf38f8c722a3ca60cb54756104a8972ff4f
ruitianzhong <zhongruitian2003@qq.com>
2022-07-04 18:12:20 +0800

    Attribute `time` is renamed as `period`

--- HttpServerWithNetty/src/main/java/top/zhongruitian/ServerWithNetty/configuration/ConfigurationPrefix.java
+++ HttpServerWithNetty/src/main/java/top/zhongruitian/ServerWithNetty/configuration/ConfigurationPrefix.java
@@ -3,7 +3,7 @@ package top.zhongruitian.ServerWithNetty.configuration;
 public class ConfigurationPrefix {
     public static final String PORT = "server.port";
     public static final String INDEX = "server.index";
-    public static final String TIME = "server.time";
+    public static final String TIME = "server.period";
 
     public static final String EXTENSIONS = "server.extensions";
 
