bdc05d4e24862f8d6315f90b6787c37a1111f802
ruitianzhong <zhongruitian2003@qq.com>
2022-07-04 19:13:06 +0800

    Remove the `the-super-tiny-redis-connector` dependency

--- src/main/java/top/zhongruitian/EcoServer/Main.java
+++ src/main/java/top/zhongruitian/EcoServer/Main.java
@@ -1,15 +1,6 @@
 package top.zhongruitian.EcoServer;
 
 
-import top.zhongruitian.Redis;
-
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello.html world!");
-        Redis redis = new Redis();
-        redis.flushAll();
-        redis.set("hello", "world");
-        System.out.println(redis.get("hello"));
-        redis.quit();
-    }
+
 }
