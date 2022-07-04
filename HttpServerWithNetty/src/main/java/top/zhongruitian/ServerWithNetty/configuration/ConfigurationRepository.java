3e19065050d275b16230184955d139552d64b864
ruitianzhong <zhongruitian2003@qq.com>
2022-07-04 12:39:41 +0800

    create the ConfigurationRepository to better manage the changes and fetches for configuration.

--- HttpServerWithNetty/src/main/java/top/zhongruitian/ServerWithNetty/configuration/ConfigurationRepository.java
+++ HttpServerWithNetty/src/main/java/top/zhongruitian/ServerWithNetty/configuration/ConfigurationRepository.java
@@ -2,12 +2,21 @@
  * Copyright 2022 ruitianzhong
  */
 package top.zhongruitian.ServerWithNetty.configuration;
+
 /**
-@author ruitianzhong
-@email zhongruitian2003@qq.com
-@date 2022/7/4 12:12
-@description
-*/
+ * @author ruitianzhong
+ * @email zhongruitian2003@qq.com
+ * @date 2022/7/4 12:12
+ * @description
+ */
 public class ConfigurationRepository {
+    private static volatile String[] Index_File_Name;
+
+    public static synchronized void setIndex_File_Name(String [] files){
+        Index_File_Name = files;
+    }
+    public static synchronized String[]getIndex_File_Name(){
+        return Index_File_Name;
+    }
 
 }
